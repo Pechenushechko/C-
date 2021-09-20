9b80daa3d56f51de7bbdf667fd3c0c0ac2f39553
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-09-20 16:33:27 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Console_welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Приветствуем " + name);
+        }
+    }
+}
