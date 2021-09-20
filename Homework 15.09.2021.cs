a86f2cad6ed312d8d8f7d6afe7dab293cb86afd2
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-09-20 14:47:04 +0300

    Update Homework 15.09.2021.cs

--- Homework 15.09.2021.cs
+++ Homework 15.09.2021.cs
@@ -7,7 +7,10 @@ namespace First_work
         static void Main(string[] args)
         {
             //Задание 1
-            /*int a = Convert.ToInt32(Console.ReadLine());
+            /*
+              Console.WriteLine("Введите первое число:");
+             int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
             int b = Convert.ToInt32(Console.ReadLine());
             if ( a == b)
             {
@@ -22,7 +25,9 @@ namespace First_work
             }*/
 
             //Задание 2
-            /*int a = Convert.ToInt32(Console.ReadLine());
+            /*
+              Console.WriteLine("Введите любое число:");
+             int a = Convert.ToInt32(Console.ReadLine());
             if(a > 5 && a < 10)
             {
                 Console.WriteLine("Число больше 5 и меньше 10");
@@ -33,7 +38,9 @@ namespace First_work
                }*/
 
             //Задание 3
-            /* int a = Convert.ToInt32(Console.ReadLine());
+            /*
+             Console.WriteLine("Введите любое число:");
+             int a = Convert.ToInt32(Console.ReadLine());
              if(a == 5 || a == 10)
              {
                  Console.WriteLine("Число либо равно 5, либо равно 10");
@@ -44,11 +51,42 @@ namespace First_work
              }*/
 
             //Задание 4
+            /*Console.WriteLine("Введите ваш вклад:");
             double a = Convert.ToDouble(Console.ReadLine());
             if (a < 100) a += a * 0.05;
             else if (a >= 100 && a < 200) a += a * 0.07;
             else a += a * 0.1;
-            Console.WriteLine(a);
+            Console.WriteLine(a);*/
+
+            //Задание 5
+            /*Console.WriteLine("Введите ваш вклад:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a < 100) a += a * 0.05;
+            else if (a >= 100 && a < 200) a += a * 0.07;
+            else a += a * 0.1;
+            Console.WriteLine(a + 15);*/
+
+            //Задания 6 
+            /*Console.WriteLine("Выберите операцию");
+            Console.WriteLine("1-Сложение 2-Вычитание 3-Умножение");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if(a == 1) {Console.WriteLine("Сложение"); }
+            else if(a == 2) { Console.WriteLine("Вычитание"); }
+            else if(a == 3) { Console.WriteLine("Умножение"); }
+            else { Console.WriteLine("Операция не определена"); }*/
+
+            //Задание 7
+            Console.WriteLine("Выберите операцию");
+            Console.WriteLine("1-Сложение 2-Вычитание 3-Умножение");
+            int opr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (opr == 1) { Console.WriteLine("Сложение"); Console.WriteLine(a + b); }
+            else if (opr == 2) { Console.WriteLine("Вычитание"); Console.WriteLine(a - b); }
+            else if (opr == 3) { Console.WriteLine("Умножение"); Console.WriteLine(a * b); } 
+            else { Console.WriteLine("Операция не определена"); }
         }
     }
 }
